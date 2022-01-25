f41161f2af1b6bf64b7c3ef62905cd216d6d7582
Dhananjay Ghevde <dhananjaybg@gmail.com>
2022-01-25 12:24:58 -0500

    yes1

---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "cluster_name" { default = "deploy_terra_dev" }
+variable "project_name" { default = "deploy_terra_project" }
+
+
+variable "atlas_org_id" { default = "5e384d3179358e03d842ead1" }
+variable "atlas_public_key" { default = "EPXIYGEQ" }
+variable "atlas_private_key" { default = "0f089e81-01ed-4e21-a0a6-7a1fdedcc8dd" }
